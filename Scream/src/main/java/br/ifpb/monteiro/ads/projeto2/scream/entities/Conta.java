56fd3c28726f252d242995aa536031ecce213dfc
Mauricio <Mauricio@Eliatrop>
2015-04-17 23:08:11 -0300

    Normalização

--- Scream/src/main/java/br/ifpb/monteiro/ads/projeto2/scream/entities/Conta.java
+++ Scream/src/main/java/br/ifpb/monteiro/ads/projeto2/scream/entities/Conta.java
@@ -6,10 +6,93 @@
 
 package br.ifpb.monteiro.ads.projeto2.scream.entities;
 
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 /**
  *
- * @author Hugo
+ * @author Mauricio
  */
-public class Conta {
+@Entity
+@Table(name = "conta")
+public class Conta implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
     
+    @OneToMany(mappedBy="id_usuario_projeto") 
+    private List<UsuarioProjeto> listUsuarioProjeto;
+          
+    @Column(nullable=false, length=30, name = "nome")
+    private String nome;
+    
+    @Column(nullable=false, length=16, name = "senha")
+    private String senha;
+    
+    @Column(nullable=false, length=10, name = "usuario")
+    private String usuario;
+    
+    @Column(nullable=false, length=40, name = "email")
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<UsuarioProjeto> getListUsuarioProjeto() {
+        return listUsuarioProjeto;
+    }
+
+    public void setListUsuarioProjeto(List<UsuarioProjeto> listUsuarioProjeto) {
+        this.listUsuarioProjeto = listUsuarioProjeto;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+    
+    
+
 }
